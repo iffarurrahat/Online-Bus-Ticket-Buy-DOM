2f6f01e355abcc522ca11f1de0c88da365409a0a
iffarurrahat <iffarurrahat@gmail.com>
2024-04-28 02:02:39 +0600

    functional part-1 updated

--- script/dom.js
+++ script/dom.js
@@ -1,11 +1,13 @@
 const allSeatBtn = document.getElementsByClassName("seat-btn");
 let count = 0;
+let totalAmount = 0;
 
 for (const seat of allSeatBtn) {
   seat.addEventListener("click", function (event) {
     // seat bg_color
     seat.style.background = "#1DD100";
     seat.style.color = "white";
+    seat.setAttribute("disabled", true);
 
     // seat count
     count = count + 1;
@@ -17,20 +19,24 @@ for (const seat of allSeatBtn) {
     setInnerText("total-seat", newTotalSeat);
 
     const seatSerial = event.target.innerText;
-    const seatPrice = 550;
     const appendContainer = document.getElementById("append-container");
     const div = document.createElement("div");
     div.innerHTML = `
         <div class="flex items-center justify-between mb-2">
             <p>${seatSerial}</p>
             <p>Economy</p>
-            <p id="total-cost">${seatPrice}</p>
+            <p id="ticket-cost">550</p>
         </div>
     `;
     appendContainer.appendChild(div);
 
-    const totalCost = parseInt(document.getElementById("total-cost").innerText);
-    const total = totalCost + seatPrice;
-    console.log(total);
+    const ticketCost = parseInt(
+      document.getElementById("ticket-cost").innerText
+    );
+    totalAmount = ticketCost + totalAmount;
+    console.log(totalAmount);
+
+    setInnerText("total-price", totalAmount);
+    setInnerText("grand-total", totalAmount);
   });
 }
